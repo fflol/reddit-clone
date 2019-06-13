fe7774e101c3b6a88d02ad520623b0bc6448ba9c
Kay Lee <kaylee1000@gmail.com>
2019-06-14 09:40:51 +1200

    14/june deleted redux dev tool for compose method

--- src/store/store.js
+++ src/store/store.js
@@ -3,10 +3,4 @@ import thunk from 'redux-thunk'
 
 import { reducer } from '../reducers/reducers'
 
-export const store = createStore(
-	reducer,
-	compose(
-		applyMiddleware(thunk),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
-)
+export const store = createStore(reducer, applyMiddleware(thunk))
