d598400affd476358f0fa7065d09b57b5ed420fd
Kay Lee <ff_lol@hotmail.com>
2019-06-13 15:36:42 +1200

    Updates

---
+++ precache-manifest.5d89795def6fb20a2d07c1db2c51a2f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6b08991369fb6db7da34f999ca577b",
+    "url": "/reddit-clone/index.html"
+  },
+  {
+    "revision": "34bdeea4ba442d0d5831",
+    "url": "/reddit-clone/static/css/main.ebb06ad6.chunk.css"
+  },
+  {
+    "revision": "33e6c2e72e759cda43a1",
+    "url": "/reddit-clone/static/js/2.81a48ba5.chunk.js"
+  },
+  {
+    "revision": "34bdeea4ba442d0d5831",
+    "url": "/reddit-clone/static/js/main.aa56ca3d.chunk.js"
+  },
+  {
+    "revision": "57805178f081ff90ec50",
+    "url": "/reddit-clone/static/js/runtime~main.483c9ea0.js"
+  },
+  {
+    "revision": "e03328ce46e77869ab15f81f2a40cbd5",
+    "url": "/reddit-clone/static/media/Reddit_logo_orange.e03328ce.svg"
+  },
+  {
+    "revision": "34dbc917151f398be695c54000d328b3",
+    "url": "/reddit-clone/static/media/banner@2x.34dbc917.png"
+  },
+  {
+    "revision": "ae56e979bb37687dfac20a175f7f1dd8",
+    "url": "/reddit-clone/static/media/reddittext.ae56e979.svg"
+  }
+]);
